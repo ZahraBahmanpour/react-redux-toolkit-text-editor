3314d039e45cb9087dd26467c123e4d13465e040
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-10 11:08:32 +0430

    add sideMenu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom";
+import SideMenu from "../components/sideMenu/SideMenu";
 function Home() {
   return (
     <div className="d-flex">
+      <SideMenu />
       <Outlet />
     </div>
   );
