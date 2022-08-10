163c36e13e9109fe4eba0ca9cc15f16b1d9055d0
ZahraBahmanpour <z.bmnpr@gmail.com>
2022-08-10 13:36:45 +0430

    add close tab

--- src/components/tabs/TabHeader.js
+++ src/components/tabs/TabHeader.js
@@ -1,20 +1,26 @@
 import { useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
 import { useNavigate } from "react-router-dom";
+import { closeTab } from "../../redux/features/tab/tabsSlice";
 import SaveModal from "../modal/Modal";
+import { useDispatch } from "react-redux";
 
 export default function TabHeader({ title, id, unSaved }) {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [isModalOpen, setIsModalOpen] = useState(false);
   const clickHandler = () => {
     navigate(`/${id}`);
+  };
+  const closeHandler = () => {
+    dispatch(closeTab({ id }));
   };
   return (
     <>
       {isModalOpen && <SaveModal show={isModalOpen} id={id} />}
       <div onClick={clickHandler}>
         {title.slice(0, 10)}...
-        <IoCloseSharp className="text-danger" />
+        <IoCloseSharp className="text-danger" onClick={closeHandler} />
       </div>
     </>
   );
--- src/redux/features/tab/tabsSlice.js
+++ src/redux/features/tab/tabsSlice.js
@@ -10,9 +10,13 @@ export const tabsSlice = createSlice({
     openTab: (state, action) => {
       return { ...state, tabs: [...state.tabs, action.payload] };
     },
+    closeTab: (state, action) => {
+      const { id } = action.payload;
+      return { ...state, tabs: state.tabs.filter((tab) => tab.id !== id) };
+    },
   },
 });
 
-export const { openTab } = tabsSlice.actions;
+export const { openTab, closeTab } = tabsSlice.actions;
 
 export default tabsSlice.reducer;
